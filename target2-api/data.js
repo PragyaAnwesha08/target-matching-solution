af7c3a1814babbc718f767711b7b01892a2f2179
PragyaAnwesha08 <pragyaanwesha2005@gmail.com>
2025-04-16 22:27:48 +0530

    Update data.js

--- target2-api/data.js
+++ target2-api/data.js
@@ -1,1 +1,8 @@
+const companies = [
+  { id: 1, companyName: 'TechCorp', matchScore: 86, accountStatus: 'Target' },
+  { id: 2, companyName: 'BizCo', matchScore: 56, accountStatus: 'Not Target' },
+  { id: 3, companyName: 'WebSoft', matchScore: 73, accountStatus: 'Target' },
+];
+
+module.exports = { companies };
 
